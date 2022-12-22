ec4ca2ec4b498dc53a10302113c12315f058d7fd
ThisaakhyaJ <thisaakhya@gmail.com>
2022-12-22 20:18:13 +0530

    Converting character variables to time

--- Lecturer data set formation.R
+++ Lecturer data set formation.R
@@ -1,24 +1,16 @@
-
 ## Packages
 library(tidyverse)
 library(lubridate)
 library(chron)
 library(hms)
 
-## Data
-timetable_data<-read.csv("timetable_data.csv", na.strings=c("","NA")) %>% as_tibble()
-
+## Data 
+timetable_cleaned_data<-read.csv("timetable_cleaned_data.csv", na.strings=c("","NA")) %>% as_tibble()
+View(timetable_cleaned_data)
 
 ## Data Wrangling
-lecturer_data<-timetable_data %>% separate(Lecturer.in.Charge, into=c("Lecturer 1", "Lecturer 2", "Lecturer 3"), sep = "-") %>%
-  pivot_longer(cols=c("Lecturer 1", "Lecturer 2", "Lecturer 3"), names_to="Lecturer Number",values_to = "Lecturer in Charge") %>%
-  select(-c("Lecturer Number")) %>% drop_na(`Lecturer in Charge`)  %>% filter(`Lecturer in Charge`!="All") 
-
-
-lecturer_data$Left_strip<- trimws(lecturer_data$`Lecturer in Charge`, which = c("left"))
-
-lecturer_data <-subset(lecturer_data, select = -c(`Lecturer in Charge`)) %>%
-  rename(`Lecturer in Charge`=Left_strip)
+lecturer_data<-timetable_cleaned_data %>% drop_na(Lecturer.in.Charge)  %>% 
+  filter(Lecturer.in.Charge!="All") %>%rename(`Lecturer in Charge`=Lecturer.in.Charge)
 
 View(lecturer_data)
 ######################################################################################
@@ -27,12 +19,13 @@ View(lecturer_data)
 
 # Starting time
 lecturer_data$Starting.Time<-hms(hours = as.numeric(substr(lecturer_data$Starting.Time, start = 1, stop = 2)),
-    minutes=as.numeric(substr(lecturer_data$Starting.Time, start = 4, stop = 5)))
+                                 minutes=as.numeric(substr(lecturer_data$Starting.Time, start = 4, stop = 5)))
 
 
 #Ending time
 lecturer_data$Ending.Time<-hms(hours = as.numeric(substr(lecturer_data$Ending.Time, start = 1, stop = 2)),
-                                 minutes=as.numeric(substr(lecturer_data$Ending.Time, start = 4, stop = 5)))
+                               minutes=as.numeric(substr(lecturer_data$Ending.Time, start = 4, stop = 5)))
+
 
 
 
