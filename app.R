f48176ee0f5074e886b04669f13e1fdb663cd690
sadrushi <sadrushil@gmail.com>
2022-12-27 14:33:28 +0530

    Imported Final_TimeTable_Data.csv file from github and prepare dataset for 1st visualization

--- app.R
+++ app.R
@@ -1,8 +1,37 @@
 ## app.R ##
 
-# loading libraries
+## loading libraries
 library(shinydashboard)
 library(shiny)
+library(tidyverse)
+library(magrittr)
+
+
+## loading data in github
+mainData <- read_csv("https://raw.githubusercontent.com/STA474Y22G1/STA474Y22fastimetable/T3/Final_TimeTable_Data.csv")
+
+
+## preparing data sets for 1st visualization
+dataSet1 <- mainData
+
+# converting Day as factor variable
+dataSet1$Day <- factor(dataSet1$Day, levels = c("Monday", "Tuesday", "Wednesday",
+                                                "Thursday", "Friday", "Saturday",
+                                                "Sunday"))
+
+# converting time variables as POSIXct
+dataSet1$Starting.Time <- as.POSIXct(dataSet1$Starting.Time)
+dataSet1$Ending.Time <- as.POSIXct(dataSet1$Ending.Time)
+
+# creating a new column for lecture time
+dataSet1 <- dataSet1 %>% mutate("Time Started" = format(as.POSIXct(dataSet1$Starting.Time,
+                                                                 format = '%m/%d/%Y %H:%M:%S'),
+                                                      format = '%H:%M'))
+dataSet1 <- dataSet1 %>% mutate("Time Ended" = format(as.POSIXct(dataSet1$Ending.Time,
+                                                               format = '%m/%d/%Y %H:%M:%S'),
+                                                    format = '%H:%M'))
+dataSet1 <- dataSet1 %>% unite("Lecture Time", c("Time Started", "Time Ended"), sep = "-")
+
 
 # user interface
 ui <- dashboardPage(
