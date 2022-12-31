0996bba40a4a53442f368ac82d47b377e6244a20
sadrushi <sadrushil@gmail.com>
2022-12-31 22:38:16 +0530

    Added course finder

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(magrittr)
 library(plotly)
 library(shinyWidgets)
-
+library(DT)
 
 ## loading data in github
 mainData <- read_csv("Course_Data.csv")
@@ -37,10 +37,16 @@ dataSet1 <- dataSet1 %>% unite("Lecture.Time", c("Time Started", "Time Ended"), 
 dataSet1 <- dataSet1 %>% pivot_longer(c("General", "Special"), "Degree.Type", "value") %>%
             filter(value == 1)
 
+# creating new column for stream
+dataSet1 <- dataSet1 %>% mutate(Stream = ifelse(Physical == 1 & Bio == 0 & Food == 0 & Sports.Science == 0, "Physical",
+                                           ifelse(Physical == 0 & Bio == 1 & Food == 0 & Sports.Science == 0, "Biology",
+                                           ifelse(Physical == 0 & Bio == 0 & Food == 1 & Sports.Science == 0, "Food Science",
+                                            ifelse(Physical == 0 & Bio == 0 & Food == 0 & Sports.Science == 1, "Sports Science",
+                                            ifelse(Physical == 1 & Bio == 1 & Food == 0 & Sports.Science == 0, "Physical/Biology", "No Stream"))))))
+
 ## creating final data set for course view
 courseViewData <- dataSet1 %>% select(-c(Index, Physical, Bio, Food, Sports.Science,
-                                         Number.of.Credits, Course.Title, Course.Code,
-                                         Semester.Half, value))
+                                         Number.of.Credits, Semester.Half, value))
 
 # to remove select all option in pickerInput function 
 my_css <- "
@@ -59,18 +65,14 @@ ui <- dashboardPage(
     
     dashboardSidebar(
         sidebarMenu(
-        menuItem(" Course View", tabName = "course_view",
-                           icon = icon(name = "book", lib="glyphicon"),
-                  menuSubItem(" Lecture View", tabName = "lecture_view",
-                             icon = icon(name = "hand-right", lib="glyphicon")),
-                   menuSubItem(" Course Finder", tabName = "course_finder",
-                    icon = icon(name = "hand-right", lib="glyphicon")))
-        )
+          menuItem(" Course View", tabName = "course_view",
+                   icon = icon(name = "book", lib="glyphicon"))
+                 )
     ),
     
     dashboardBody(
       tabItems(
-        tabItem(tabName = "lecture_view",
+        tabItem(tabName = "course_view",
                 fluidRow(
                   box(width = 4,
                       
@@ -100,10 +102,12 @@ ui <- dashboardPage(
                     ),
                   
                   # 1st visualization
-                  box(plotlyOutput("viz1", height = 300), width = 8)
+                  box(plotlyOutput("viz1", height = 250), width = 8),
+                  
+                  # Course finder table
+                  box(dataTableOutput("CourseData", height = 250), width = 12)
            )
-         ),
-        tabItem(tabName = "course_finder", h2('course finder tab'))
+         )
       )
    )
 )
@@ -148,9 +152,8 @@ server <- function(input, output, session){
   output$viz1 <- renderPlotly({
     
     plot1 <- subject() %>% ggplot(aes(label1 = Course, 
-                                   label2 = Lecturer.in.charge,
-                                   label3 = Lecture.Time,
-                                   label4 = Location)) + 
+                                      label2 = Lecture.Time,
+                                      label3 = Location)) + 
       geom_linerange(aes(x = Starting.Time, xmin = Starting.Time, 
                          xmax = Ending.Time, y = Day, color = Subject.Code), 
                      linewidth = 2, position = position_dodge(0.5)) +
@@ -162,9 +165,34 @@ server <- function(input, output, session){
       labs(title = "Lecture Hours", color = "Subject") 
     
     # interactive plot
-    ggplotly(plot1, tooltip = c("label1", "label2", "label3", "label4"))
+    ggplotly(plot1, tooltip = c("label1", "label2", "label3"))
       
   })
+  
+  # course finder
+  # Data set for course finder
+  course <- reactive({
+    subject() %>% filter(Lecture.Type == "Lecture") %>% 
+      select(Department, Stream, Course.Code, Course.Title, 
+             Lecturer.in.charge) %>%
+      distinct(Course.Code, Course.Title, .keep_all = TRUE)
+  })
+  
+  
+  # Course finder table
+  output$CourseData <- renderDataTable(course(),
+                                       options = list(paging = TRUE,
+                                                      pageLength = 4,
+                                                      dom = 'tip'),
+                                       colnames = c('Course Code' = 'Course.Code',
+                                                    'Course Title' = 'Course.Title',
+                                                    'Lecturer in Charge' = 'Lecturer.in.charge'),
+                                       caption = htmltools::tags$caption(
+                                         style = 'caption-side: top; text-align: left; font-style: normal; 
+                                         color: black; font-family: arial; font-size: 1.8rem;',
+                                         htmltools::em('Course Finder'))
+                                       )
+  
   
 }
 
